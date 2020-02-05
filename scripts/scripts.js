c99d980e58659293174d794f42312c7f802802a0
maggieumber <maggieumber@gmail.com>
2020-02-05 09:06:56 -0600

    added a new mini game with JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,12 @@
 function finishFunction() {
   alert("O no! Piper ate your Quiz results.")
 }
+
+// Hide & Sneak
+function show_hide() {
+  if (document.getElementById("appear").style.display == 'none') {
+    document.getElementById("appear").style.display = 'block';
+  } else {
+    document.getElementById("appear").style.display = 'none';
+  }
+}
